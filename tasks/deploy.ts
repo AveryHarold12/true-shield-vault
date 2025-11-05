6ed9c37aef4beb0a8c6ab30e61945adeedff4d48
StanfordCoverdale <porchsiketz1@outlook.com>
2025-11-05 09:00:00 -0800

    feat: add Hardhat deployment task for GamePreferenceSurvey contract

---
+++ tasks/deploy.ts
@@ -0,0 +1,15 @@
+﻿import { task } from "hardhat/config";
+
+task("deploy-survey", "Deploy GamePreferenceSurvey contract")
+  .setAction(async (taskArgs, hre) => {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const GamePreferenceSurvey = await hre.ethers.getContractFactory("GamePreferenceSurvey");
+    const survey = await GamePreferenceSurvey.deploy();
+
+    await survey.waitForDeployment();
+
+    console.log("GamePreferenceSurvey deployed to:", await survey.getAddress());
+  });
